47596b3058bc2b7ca5ad89f853c6b74ed3f16e08
babloo-n <121771549+babloo-n@users.noreply.github.com>
2023-05-19 01:18:20 +0300

    Update stop.py

--- AnonX/plugins/stop.py
+++ AnonX/plugins/stop.py
@@ -14,7 +14,7 @@ STOP_COMMAND = get_command("STOP_COMMAND")
 
 
 @app.on_message(
-    filters.command(STOP_COMMAND)
+    filters.command(["ايقاف","/end","انهاء"])
     & filters.group
     & ~filters.edited
     & ~BANNED_USERS
