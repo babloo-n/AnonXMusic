1e1706522e790d13ba280a826cd93f5b1c45563b
babloo-n <121771549+babloo-n@users.noreply.github.com>
2023-05-20 09:19:16 +0300

    Update stop.py

--- AnonX/plugins/stop.py
+++ AnonX/plugins/stop.py
@@ -3,6 +3,7 @@ from pyrogram.types import Message
 
 from config import BANNED_USERS
 from strings import get_command
+from strings.filters import command
 from AnonX import app
 from AnonX.core.call import Anon
 from AnonX.utils.database import set_loop
@@ -14,10 +15,17 @@ STOP_COMMAND = get_command("STOP_COMMAND")
 
 
 @app.on_message(
-    filters.command(["ايقاف","/end","انهاء"])
+    filters.command(STOP_COMMAND)
     & filters.group
     & ~filters.edited
     & ~BANNED_USERS
+)
+@app.on_message(
+   command(["ايقاف","/end","انهاء"])
+    & filters.group
+    & ~filters.edited
+    & ~BANNED_USERS
+
 )
 @AdminRightsCheck
 async def stop_music(cli, message: Message, _, chat_id):
