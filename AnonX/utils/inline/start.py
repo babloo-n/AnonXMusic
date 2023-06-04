e349b80875369c556b3c41986e11b490ac76d78e
babloo-n <121771549+babloo-n@users.noreply.github.com>
2023-06-04 12:56:15 +0300

    Update start.py

--- AnonX/utils/inline/start.py
+++ AnonX/utils/inline/start.py
@@ -49,7 +49,7 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
         ],
         [
             InlineKeyboardButton(
-                text="✨ sᴏᴜʀᴄᴇ ✨", url=f"https://t.me/Hour_W33d"
+                text="✨ sᴏᴜʀᴄᴇ ✨", url=f"https://t.me/Mzboooi"
             )
         ],
      ]
